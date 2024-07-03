68813f9a11343ed1e2fe3d30a378beed5c98a1d5
RetracedFn <retracedfn@gmail.com>
2024-07-03 06:08:04 -0700

    zaplines and fixes idk if work

--- Beyond-13.40/Player.cpp
+++ Beyond-13.40/Player.cpp
@@ -21,6 +21,46 @@ void Player::ServerLoadingScreenDroppedHook(AFortPlayerControllerAthena* PC)
 	return ServerLoadingScreenDropped(PC);
 }
 
+bool IsPrimaryQuickbar(UFortItemDefinition* Def)
+{
+	return Def->IsA(UFortConsumableItemDefinition::StaticClass()) || Def->IsA(UFortWeaponRangedItemDefinition::StaticClass()) || Def->IsA(UFortGadgetItemDefinition::StaticClass());
+}
+
+void (*ServerHandlePickupOG)(AFortPlayerPawn* Pawn, AFortPickup* Pickup, float InFlyTime, FVector InStartDirection, bool bPlayPickupSound);
+void ServerHandlePickup(AFortPlayerPawn* Pawn, AFortPickup* Pickup, float InFlyTime, FVector InStartDirection, bool bPlayPickupSound)
+{
+	if (Pickup->bPickedUp)
+		return ServerHandlePickupOG(Pawn, Pickup, InFlyTime, InStartDirection, bPlayPickupSound);
+
+	AFortPlayerControllerAthena* PC = (AFortPlayerControllerAthena*)Pawn->Controller;
+	if (PC->IsA(ABP_PhoebePlayerController_C::StaticClass())) // AAIController
+		return;
+	UFortItemDefinition* Def = Pickup->PrimaryPickupItemEntry.ItemDefinition;
+	int PickupCount = Pickup->PrimaryPickupItemEntry.Count;
+	int PickupLoadedAmmo = Pickup->PrimaryPickupItemEntry.LoadedAmmo;
+	FFortItemEntry* FoundEntry = nullptr;
+	int Items = 0;
+	//float MaxStackSize = GetMaxStackSize(Def);
+	bool Stackable = Def->IsStackable();
+
+	if (!Pawn->CurrentWeapon)
+		return;
+
+	for (size_t i = 0; i < PC->WorldInventory->Inventory.ReplicatedEntries.Num(); i++)
+	{
+		FFortItemEntry& Entry = PC->WorldInventory->Inventory.ReplicatedEntries[i];
+
+		if (IsPrimaryQuickbar(Def) && IsPrimaryQuickbar(Entry.ItemDefinition))
+		{
+			Items++;
+			if (Items > 5)
+			{
+				//Remove(PC, Entry.ItemGuid);
+			}
+		}
+	}
+}
+
 void Player::HookAll()
 {
 	auto PlayerControllerAthenaDefault = StaticFindObject<AFortPlayerControllerAthena>("/Script/FortniteGame.Default__FortPlayerControllerAthena");
